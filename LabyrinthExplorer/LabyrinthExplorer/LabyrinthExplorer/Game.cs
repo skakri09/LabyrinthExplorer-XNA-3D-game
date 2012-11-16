a82f784802946b42720aae62fad7d48625f254b3
Kristian Skarseth <kristianskarseth@msn.com>
2012-11-16 21:23:44 +0100

    Implementing stuff

--- LabyrinthExplorer/LabyrinthExplorer/LabyrinthExplorer/Game.cs
+++ LabyrinthExplorer/LabyrinthExplorer/LabyrinthExplorer/Game.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,14 +23,11 @@ namespace LabyrinthExplorer
 
         InputManager input;
         Camera cam;
+        Model torch;
+        Matrix[] torchTransforms;
+        Matrix torchWorldMatrix;
 
-        Model model;
-        float aspectRatio;
-
-        Vector3 modelPos = Vector3.Zero;
-        float modelRot = 0.0f;
-  
-        
+        Model wall;
 
         public Game()
         {
@@ -40,10 +38,10 @@ namespace LabyrinthExplorer
             Content.RootDirectory = "Content";
 
             input = new InputManager(this);
-            Services.AddService(typeof(IInputService), input);
             input.SetMouseVisible(true);
 
             cam = new Camera(this);
+            Components.Add(cam);
         }
 
         /// <summary>
@@ -54,8 +52,28 @@ namespace LabyrinthExplorer
         /// </summary>
         protected override void Initialize()
         {
-            
             base.Initialize();
+            // These camera settings are application specific.
+            // Your values will differ to the ones used here.
+            // It's important that your application correctly initializes
+            // the camera's EyeHeightStanding, Acceleration, VelocityWalking,
+            // and VelocityRunning properties. These properties are used
+            // to move the camera about in your virtual 3D world.
+            cam.EyeHeightStanding = 100.0f;
+            cam.Acceleration = new Vector3(800.0f, 800.0f, 800.0f);
+            cam.VelocityWalking = new Vector3(200.0f, 200.0f, 200.0f);
+            cam.VelocityRunning = cam.VelocityWalking * 2.0f;
+
+            int width = this.graphics.GraphicsDevice.DisplayMode.Width / 2;
+            int height = this.graphics.GraphicsDevice.DisplayMode.Height / 2;
+            float aspectRatio = (float)width / (float)height;
+
+            // Setup the camera's perspective projection matrix.
+            cam.Perspective(90.0f, aspectRatio, 0.01f, 1000.0f);
+
+            // Initialize the weapon matrices.
+            torchTransforms = new Matrix[torch.Bones.Count];
+            torchWorldMatrix = Matrix.Identity;
         }
 
         /// <summary>
@@ -67,9 +85,10 @@ namespace LabyrinthExplorer
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            
-            model = Content.Load<Model>("Models\\Wall");
-            aspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
+            Services.AddService(typeof(IInputService), input);
+            wall = Content.Load<Model>("Models\\Wall");
+            torch = Content.Load<Model>("Models\\LightStick");
+
         }
 
         /// <summary>
@@ -96,9 +115,7 @@ namespace LabyrinthExplorer
             input.Update();
             if (input.IsKeyDown(Keys.Escape))
                 this.Exit();
-            cam.Update(deltaTime);
 
-            modelRot += (float)gameTime.ElapsedGameTime.TotalMilliseconds * MathHelper.ToRadians(0.1f);
 
             base.Update(gameTime);
         }
@@ -111,22 +128,21 @@ namespace LabyrinthExplorer
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            Matrix[] transforms = new Matrix[model.Bones.Count];
-            model.CopyAbsoluteBoneTransformsTo(transforms);
+            //Matrix[] transforms = new Matrix[wall.Bones.Count];
+            //wall.CopyAbsoluteBoneTransformsTo(transforms);
+            torch.CopyAbsoluteBoneTransformsTo(torchTransforms);
+            torchWorldMatrix = cam.WeaponWorldMatrix(0.45f, -0.30f, 1.65f,0.5f);
 
-            foreach (ModelMesh mesh in model.Meshes)
+
+
+            foreach (ModelMesh mesh in torch.Meshes)
             {
                 foreach (BasicEffect effect in mesh.Effects)
                 {
                     effect.EnableDefaultLighting();
-                    effect.World = transforms[mesh.ParentBone.Index] *
-                        Matrix.CreateRotationY(modelRot)
-                        * Matrix.CreateTranslation(modelPos);
-
-
-                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(
-                        MathHelper.ToRadians(45.0f), aspectRatio,
-                        1.0f, 10000.0f);
+                    effect.World = torchTransforms[mesh.ParentBone.Index] * torchWorldMatrix;
+                    effect.View = cam.ViewMatrix;
+                    effect.Projection = cam.ProjectionMatrix;
                 }
                 mesh.Draw();
             }
